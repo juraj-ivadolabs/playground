8bc5a69c90afc7f19013eb2fbeece4e876448f80
Juraj Hreško <juraj.hresko@gmail.com>
2021-04-29 18:48:17 -0400

    Disable failing test

--- test/unit/module/test_module.py
+++ test/unit/module/test_module.py
@@ -1,7 +1,7 @@
 from module.module import add_one
 
-def test_wrong_answer():
-    assert add_one(3) == 5
+# def test_wrong_answer():
+#     assert add_one(3) == 5
 
 def test_correct_answer():
     assert add_one(1) == 2
